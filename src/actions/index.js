57a9a4bf3567a800ddc60df792564686cc197e45
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-28 10:41:01 +0530

    Added action module of the redux cycle

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+// an example of action.
+{
+  type: "INCREASE_COUNT";
+  movies: [];
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+// reducer function
+export default function movies(state = [], action) {
+  // checking the type of function to perform a specific operation
+  if (action.type === "ADD_MOVIES") {
+    return action.movies;
+  }
+
+  // if action type does not match then return the original or old state
+  return state;
+}
